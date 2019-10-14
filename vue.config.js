14d7866e7391b77f3ed8d7c457ae3baebba6330c
Felipe Cadavid <felipe@digifit.eu>
2019-10-14 15:02:45 -0500

    added path

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // filenameHashing: false,
+  // productionSourceMap: true,
+  // css: {
+  //   sourceMap: true,
+  // },
+  publicPath: '/',
+  // chainWebpack: (config) => {
+  //   config.optimization.splitChunks(false)
+
+  //   if (config.plugins.has('extract-css')) {
+  //     const extractCSSPlugin = config.plugin('extract-css')
+  //     if (extractCSSPlugin) {
+  //       extractCSSPlugin.tap(() => [{
+  //         filename: 'cfcss/[name].css',
+  //         chunkFilename: 'cfcss/[name].css',
+  //       }])
+  //     }
+  //   }
+  // },
+  // configureWebpack: {
+  //   devtool: 'eval-source-map',
+  //   output: {
+  //     filename: 'cfjs/[name].js',
+  //     chunkFilename: 'cfjs/[name].js',
+  //   },
+  // },
+  // transpileDependencies: [/node_modules[/\\\\]vuetify[/\\\\]/],
+}
